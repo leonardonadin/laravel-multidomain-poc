1b5759cd69499cb0faeed91717b6add45ae2b7e9
Leonardo Nadin <nadin.leonardo@gmail.com>
2023-02-01 16:28:19 -0300

    Adiciona texto simples na home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,9 @@
 @section('content')
     <div class="container">
         <h2 class="block text-center mt-5 text-lg mx-auto font-large text-black">You are on {{ $account->name }}</h1>
+        <p>
+            Welcome to your account. You can manage your products here.
+        </p>
         <x-product-list />
     </div>
 @endsection
