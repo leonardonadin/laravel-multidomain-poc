93da4cadc57bd2418fadff3a5d2485b526c84b1d
Leonardo Nadin <nadin.leonardo@gmail.com>
2023-02-01 10:17:44 -0300

    Melhora seeders

--- database/seeders/AccountSeeder.php
+++ database/seeders/AccountSeeder.php
@@ -16,15 +16,31 @@ class AccountSeeder extends Seeder
     public function run()
     {
         DB::table('accounts')->insert([
-            'name' => 'Account 1',
-            'domain' => 'account1.localhost',
-            'subdomain' => 'account1',
-        ]);
-
-        DB::table('accounts')->insert([
-            'name' => 'Account 2',
-            'domain' => 'account2.localhost',
-            'subdomain' => 'account2',
+            [
+                'name' => 'Account 1',
+                'domain' => 'account1.localhost',
+                'subdomain' => 'account1',
+            ], [
+                'name' => 'Account 2',
+                'domain' => 'account2.localhost',
+                'subdomain' => 'account2',
+            ], [
+                'name' => 'AlcStore',
+                'domain' => 'alcstore.com.br',
+                'subdomain' => 'alcstore',
+            ], [
+                'name' => 'AlcStore - Temporário',
+                'domain' => 'alcstore.web15f71.uni5.net',
+                'subdomain' => 'alcstore.web15f71',
+            ], [
+                'name' => 'Qual o App?',
+                'domain' => 'qualoapp.com.br',
+                'subdomain' => 'qualoapp',
+            ], [
+                'name' => 'Alcance',
+                'domain' => 'alcance.dev',
+                'subdomain' => 'alcance',
+            ]
         ]);
     }
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -25,56 +25,19 @@ class ProductSeeder extends Seeder
             'description' => 'Product 2 description',
         ]);
 
-        DB::table('product_prices')->insert([
-            'product_id' => 1,
-            'price' => 10.00,
-            'promo_price' => 5.00,
-            'account_id' => 1,
-        ]);
+        foreach (DB::table('accounts')->get() as $account) {
+            DB::table('product_prices')->insert([
+                'product_id' => 1,
+                'price' => rand(0, 100),
+                'promo_price' => rand(0, 100),
+                'account_id' => $account->id,
+            ]);
 
-        DB::table('product_prices')->insert([
-            'product_id' => 1,
-            'price' => 20.00,
-            'promo_price' => 10.00,
-            'account_id' => 2,
-        ]);
-
-        DB::table('product_prices')->insert([
-            'product_id' => 2,
-            'price' => 30.00,
-            'promo_price' => 15.00,
-            'account_id' => 1,
-        ]);
-
-        DB::table('product_prices')->insert([
-            'product_id' => 2,
-            'price' => 40.00,
-            'promo_price' => 20.00,
-            'account_id' => 2,
-        ]);
-
-        DB::table('product_stocks')->insert([
-            'product_id' => 1,
-            'quantity' => 10,
-            'account_id' => 1,
-        ]);
-
-        DB::table('product_stocks')->insert([
-            'product_id' => 1,
-            'quantity' => 20,
-            'account_id' => 2,
-        ]);
-
-        DB::table('product_stocks')->insert([
-            'product_id' => 2,
-            'quantity' => 30,
-            'account_id' => 1,
-        ]);
-
-        DB::table('product_stocks')->insert([
-            'product_id' => 2,
-            'quantity' => 40,
-            'account_id' => 2,
-        ]);
+            DB::table('product_stocks')->insert([
+                'product_id' => 2,
+                'quantity' => rand(0, 100),
+                'account_id' => $account->id,
+            ]);
+        }
     }
 }
